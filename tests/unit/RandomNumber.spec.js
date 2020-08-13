54b37437c03f40f29a005d2b20d5dfc309cd8777
Abel Masila <abelmasila@gmail.com>
2020-08-13 17:34:40 +0300

    test user interactions and props

--- tests/unit/RandomNumber.spec.js
+++ tests/unit/RandomNumber.spec.js
@@ -3,14 +3,34 @@ import RandomNumber from '@/components/RandomNumber'
 
 describe('RandomNumber', () => {
   test('By default, randomNumber data value should be 0', () => {
-    expect(true).toBe(false)
+    const wrapper = mount(RandomNumber)
+    expect(wrapper.html()).toContain('<span>0</span>')
   })
 
-  test('If button is clicked, randomNumber should be between 1 and 10', () => {
-    expect(true).toBe(false)
+  test('If button is clicked, randomNumber should be between 1 and 10', async () => {
+    const wrapper = mount(RandomNumber)
+    wrapper.find('button').trigger('click')
+
+    await wrapper.vm.$nextTick()
+
+    const randomNumber = parseInt(wrapper.find('span').element.textContent)
+    expect(randomNumber).toBeGreaterThanOrEqual(1)
+    expect(randomNumber).toBeLessThanOrEqual(10)
   })
 
-  test('If button is clicked, randomNumber should be between 200 and 300', () => {
-    expect(true).toBe(false)
+  test('If button is clicked, randomNumber should be between 200 and 300', async () => {
+    const wrapper = mount(RandomNumber, {
+      propsData: {
+        min: 200,
+        max: 300
+      }
+    })
+    wrapper.find('button').trigger('click')
+
+    await wrapper.vm.$nextTick()
+
+    const randomNumber = parseInt(wrapper.find('span').element.textContent)
+    expect(randomNumber).toBeGreaterThanOrEqual(200)
+    expect(randomNumber).toBeLessThanOrEqual(300)
   })
 })
