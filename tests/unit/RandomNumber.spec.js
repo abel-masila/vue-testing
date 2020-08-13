c8fd15516dfe1c70def4f6ae973aed076950c5f9
Abel Masila <abelmasila@gmail.com>
2020-08-13 17:27:36 +0300

    added RandomNumber component and test suite

---
+++ tests/unit/RandomNumber.spec.js
@@ -0,0 +1,16 @@
+import { mount } from '@vue/test-utils'
+import RandomNumber from '@/components/RandomNumber'
+
+describe('RandomNumber', () => {
+  test('By default, randomNumber data value should be 0', () => {
+    expect(true).toBe(false)
+  })
+
+  test('If button is clicked, randomNumber should be between 1 and 10', () => {
+    expect(true).toBe(false)
+  })
+
+  test('If button is clicked, randomNumber should be between 200 and 300', () => {
+    expect(true).toBe(false)
+  })
+})
